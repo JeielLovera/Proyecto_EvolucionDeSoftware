3a28ea16d483d44bd9c5d0b514b03ed39409e09f
JeielLovera <jeiel.disas@gmail.com>
2019-11-17 16:41:40 -0500

    funcion findByCenfermero Repository,Service y Controller

--- 06 Implementacion/BackEnd/src/main/java/pe/edu/upc/empresa/service/EnfermeroEspecialidadService.java
+++ 06 Implementacion/BackEnd/src/main/java/pe/edu/upc/empresa/service/EnfermeroEspecialidadService.java
@@ -1,7 +1,10 @@
 package pe.edu.upc.empresa.service;
 
+import java.util.List;
+
+import pe.edu.upc.empresa.model.entity.Enfermero;
 import pe.edu.upc.empresa.model.entity.EnfermeroEspecialidad;
 
 public interface EnfermeroEspecialidadService extends CrudService<EnfermeroEspecialidad, Integer> {
-
+	List<EnfermeroEspecialidad> findByCenfermero(Enfermero cenfermero) throws Exception; 
 }
